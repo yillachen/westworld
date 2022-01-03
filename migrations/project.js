b864edab9fd7c4a3e51bfe2e45ab818f663241e0
yillachen <chn.yilla@gmail.com>
2022-01-03 15:34:22 -0500

    initial commit

---
+++ migrations/project.js
@@ -0,0 +1,41 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Books", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: false,
+        },
+      },
+      deadline: {
+        type: Sequelize.DATE,
+      },
+      priority: {
+        type: Sequelize.INTEGER,
+        defaultValue: 5,
+        validate: {
+          min: 1,
+          max: 10,
+        },
+      },
+      completed: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Books");
+  },
+};
